23d214269abe72730b9454ebefac1d78734f5277
nicollas-provatti <npg@ic.ufal.br>
2026-04-15 10:11:27 -0300

    Update script.js

---
+++ Aula 13/script.js
@@ -0,0 +1,5 @@
+const btnTheme = document.getElementById("btn-theme");
+const html = document.documentElement;
+const btnOpenNavMobile = document.getElementById("btn-open");
+const btnCloseNavMobile = document.getElementById("btn-close");
+const navMobile = document.getElementById("nav-mobile");
